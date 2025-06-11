087d089ba9c0ec426095d3e5733bdc0e22e76536
RyoHaSeu99 <cmrhanc123@naver.com>
2025-06-11 09:44:19 +0900

    생성자 1

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,24 @@
-public class Main {
-    public static int addNumber (int a, int b) {
-        int result = a + b;
-        return result;
+class Person {
+    String name;
+    int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
     }
 
+    public void sayHello() {
+        System.out.println("안녕하세요 저는 " + name + "이고, 나이는 " + age + "입니다.");
+    }
+}
+
+
+public class Main {
     public static void main(String[] args) {
-        int number = addNumber(10, 20);
-        System.out.println(number);
+        Person p1 = new Person("한철규", 27);
+        Person p2 = new Person("신태희", 24);
+        p1.sayHello();
+        p2.sayHello();
+
     }
 }
