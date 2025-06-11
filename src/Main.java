398dae929e01be2e470e9163be83d2ec2cf485b5
RyoHaSeu99 <cmrhanc123@naver.com>
2025-06-11 10:36:47 +0900

    출력함수1

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,11 @@
-class Car {
-    String brand;
-    int year;
-
-
-    public Car(String brand, int year) {
-        this.brand = brand;
-        this.year = year;
-    }
-
-
-    public void displayInfo() {
-
-            System.out.println(brand);
-            System.out.println(year);
-        }
-
-
-}
-
-
 public class Main {
     public static void main(String[] args) {
-        Car car1 = new Car("현대", 20);
-        Car car2 = new Car("기아", 24);
-
-        car1.displayInfo();
-        car2.displayInfo();
+        System.out.print("프린트 함수랑");
+        System.out.println("프린트 ln 함수의");
+        System.out.print("차이점을 찾아서");
+        System.out.println("디스코드 채팅으로");
+        System.out.print("알려주시면");
+        System.out.println();
+        System.out.println("좋을것 같습니다.");
     }
 }
