f1c0876f07a2670dec7bb5bd2090eb6a9ed17b81
RyoHaSeu99 <cmrhanc123@naver.com>
2025-06-12 10:22:29 +0900

    상속 1

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,32 @@
-import java.util.Scanner;
+class Animal {
+    String name;
+
+    public Animal(String name) {
+        this.name = name;
+    }
+
+    public void printName(String name) {
+        System.out.println("Animal name: " + name + this.name);
+    }
+
+}
+
+class Dog extends Animal {
+    // String name;     생략 가능
+    String breed;
+
+    public Dog(String name, String breed) {
+        super(name);
+        this.breed = breed;
+    }
+}
+
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Dog dog = new Dog("코코", "말티즈");
 
-        System.out.println("나이를 입력하세요: ");
-        int age = scanner.nextInt();
-
-       scanner.nextLine();
-
-
-        System.out.println("이름을 입력하세요: ");
-        String name = scanner.nextLine();
-
-        System.out.printf("나이 %d, 이름 %s", age, name);
-
-
+        dog.printName("쿠키");
 
     }
 }
