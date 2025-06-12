4702fbe58da34269ecc495ef89ccec2077dd8ca9
RyoHaSeu99 <cmrhanc123@naver.com>
2025-06-12 13:04:08 +0900

    오버드라이브

--- src/Main.java
+++ src/Main.java
@@ -3,30 +3,38 @@ class Animal {
 
     public Animal(String name) {
         this.name = name;
+
     }
 
-    public void printName(String name) {
-        System.out.println("Animal name: " + name + this.name);
+    public void printName() {
+        System.out.println("이름: " + name);
     }
-
 }
 
 class Dog extends Animal {
-    // String name;     생략 가능
     String breed;
 
     public Dog(String name, String breed) {
         super(name);
         this.breed = breed;
     }
+
+    @Override
+    public void printName() {
+        System.out.println("이름: " + name);
+        System.out.println("종: " + breed);
+    }
+
+
 }
 
 
 public class Main {
     public static void main(String[] args) {
         Dog dog = new Dog("코코", "말티즈");
-
-        dog.printName("쿠키");
+        dog.printName();
+        
 
     }
 }
+
