75383714004ff97e8eabbfee776af3266677030e
RyoHaSeu99 <cmrhanc123@naver.com>
2025-06-13 09:35:35 +0900

    public test

--- src/Main.java
+++ src/Main.java
@@ -1,40 +1,4 @@
-class Animal {
-    String name;
-
-    public Animal(String name) {
-        this.name = name;
-
-    }
-
-    public void printName() {
-        System.out.println("이름: " + name);
-    }
-}
-
-class Dog extends Animal {
-    String breed;
-
-    public Dog(String name, String breed) {
-        super(name);
-        this.breed = breed;
-    }
-
-    @Override
-    public void printName() {
-        System.out.println("이름: " + name);
-        System.out.println("종: " + breed);
-    }
-
-
-}
-
-
 public class Main {
     public static void main(String[] args) {
-        Dog dog = new Dog("코코", "말티즈");
-        dog.printName();
-        
-
     }
 }
-
