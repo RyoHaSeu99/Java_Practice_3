ad56424e398a15b7486c6c61e8e2415b44ee3d1c
RyoHaSeu99 <cmrhanc123@naver.com>
2025-06-17 23:53:29 +0900

    다형성

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,39 @@
-class Person {
-    public static void greet() {
-        System.out.println("hello from static method!");
-}
-    public void sayHello() {
-        System.out.println("Hello from instance method!");
+class Animal {
+    void makeSound() {
+        System.out.println("울음 소리");
     }
-    public static void main(String[] args) {
-        Person.greet();
+}
 
-        Person p = new Person();
-        p.sayHello();
+class Dog extends Animal {
+    @Override
+    void makeSound() {
+        System.out.println("멍멍!");
+    }
+}
+
+class Cat extends Animal {
+    @Override
+    void makeSound() {
+        System.out.println("야옹!");
+    }
+}
+
+class Bird extends Animal {
+    @Override
+    void makeSound() {
+        System.out.println("꽤액!");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Animal a1 = new Dog();
+        Animal a2 = new Cat();
+        Animal a3 = new Bird();
+
+        a1.makeSound();
+        a2.makeSound();
+        a3.makeSound();
+
     }
 }
