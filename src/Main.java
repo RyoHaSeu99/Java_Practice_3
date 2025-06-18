2848bdc91e9fa1ce5585311e0790689ec61fe7c8
RyoHaSeu99 <cmrhanc123@naver.com>
2025-06-18 19:01:14 +0900

    인터페이스 2

--- src/Main.java
+++ src/Main.java
@@ -5,14 +5,32 @@ interface Animal {
 class Dog implements Animal {
     @Override
     public void makeSound() {
-        System.out.println("Bark!");
+        System.out.println("멍멍!");
+    }
+}
+
+class Cat implements Animal {
+    public void makeSound() {
+        System.out.println("야옹!");
+    }
+}
+
+class Cow implements Animal {
+    public void makeSound() {
+        System.out.println("음메!");
     }
 }
 
 public class Main {
     public static void main(String[] args) {
-        Animal dog = new Dog();
+        Animal[] animals = {
+            new Dog(),
+            new Cat(),
+            new Cow()
+        };
 
-        dog.makeSound();
+        for (Animal a : animals) {
+            a.makeSound();
+        }
     }
 }
