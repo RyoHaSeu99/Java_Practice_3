456fb20be190ff146e31bb3fed5114a6f621582e
RyoHaSeu99 <cmrhanc123@naver.com>
2025-06-18 18:52:27 +0900

    인터페이스 1

--- src/Main.java
+++ src/Main.java
@@ -1,44 +1,18 @@
-abstract class Shape {
-    abstract double area();
+interface Animal {
+    void makeSound();
 }
 
-class Circle extends Shape {
-    private double radius;
-
-    public Circle(double radius) {
-        this.radius = radius;
-    }
-
+class Dog implements Animal {
     @Override
-    double area() {
-        return Math.PI * radius * radius;
-    }
-}
-
-class rectangle extends Shape {
-    private double width;
-    private double height;
-
-    public rectangle(double width, double height) {
-        this.width = width;
-        this.height = height;
-    }
-
-    @Override
-    double area() {
-        return width * height;
+    public void makeSound() {
+        System.out.println("Bark!");
     }
 }
 
 public class Main {
     public static void main(String[] args) {
-        Shape[] shapes = new Shape[2];
+        Animal dog = new Dog();
 
-        shapes[0] = new Circle(3);
-        shapes[1] = new rectangle(4, 5);
-
-        for (Shape s : shapes) {
-            System.out.println(s.area());
-        }
+        dog.makeSound();
     }
 }
