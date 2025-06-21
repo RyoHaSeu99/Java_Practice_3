8c48da144419e949ee6d7b7b6723b67ac8d19098
RyoHaSeu99 <cmrhanc123@naver.com>
2025-06-21 16:11:57 +0900

    Inner Class 4

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,19 @@
-public class Main {
-    String outerMessage = "Hello from Outer";
-
-    class Inner {
-        void showMessage() {
-            System.out.println(outerMessage);
+class Outer {
+    private class HiddenInner {
+        void secret() {
+            System.out.println("This is private!");
         }
     }
+
+    public void reveal() {
+        HiddenInner hi = new HiddenInner();
+        hi.secret();
+    }
+}
+
+public class Main {
     public static void main(String[] args) {
-        // 1. Outer 객체 생성
-        Main outer = new Main();
-
-        // 2. Inner 객체 생성
-        Main.Inner inner = outer.new Inner();
-
-        // 3. Inner 클래스 메서드 호출
-        inner.showMessage();
+        Outer outer = new Outer();
+        outer.reveal();
     }
 }
