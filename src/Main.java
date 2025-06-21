e5e022de4c4f9819f87ec9e86f7f9aa57c2373c6
RyoHaSeu99 <cmrhanc123@naver.com>
2025-06-21 15:16:42 +0900

    Inner Class 1

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,18 @@
-class Person {
-    static String a = "static";
-    String b = "instance";
-
-    public static void greed() {
-        System.out.println("Hello from static method!");
-    }
-
-    public void sayHello() {
-        System.out.println("Hello from instance method!");
-    }
-}
-
 public class Main {
-    public static void main(String[] args) {
-        // static 멤버는 클래스 이름으로 직접 호출
-        Person.greed();
-        System.out.println(Person.a);
+    private String message = "Hello from the outer class!";
 
-        // 인스턴스 멤버는 객체 생성 후 객체 이름으로 호출
-        Person p = new Person();
-        p.sayHello();
-        System.out.println(p.b);
+    class InnerClass {
+        private void displayMessage() {
+            System.out.println("Inner says: " + message);
+        }
+    }
+
+    public void callInner() {
+        InnerClass inner = new InnerClass();
+        inner.displayMessage();
+    }
+    public static void main(String[] args) {
+        Main main = new Main();
+        main.callInner();
     }
 }
