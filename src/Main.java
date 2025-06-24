830c3c79295ef2a0c8fac2321bbd638ddfefed7f
RyoHaSeu99 <cmrhanc123@naver.com>
2025-06-24 13:47:59 +0900

    Anonymous Class

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+interface Calculator {
+    int compute(int a, int b);
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Calculator calc = new Calculator() {
+            @Override
+            public int compute(int a, int b) {
+                return a * b;
+            }
+        };
+        System.out.println(calc.compute(3, 4));
+    }
+}
+
