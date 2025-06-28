8aadaafe87857906584db00a3abf506839608979
RyoHaSeu99 <cmrhanc123@naver.com>
2025-06-28 12:59:56 +0900

    TreeSet

--- src/Main.java
+++ src/Main.java
@@ -2,18 +2,12 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        List<String> fruits = new ArrayList<>();
+        Set<String> treedFruits = new TreeSet<>();
 
-        fruits.add("Banana");
-        fruits.add("Mango");
-        fruits.add("Apple");
+        treedFruits.add("Banana");
+        treedFruits.add("Mango");
+        treedFruits.add("Apple");
 
-        System.out.println(fruits.indexOf("Banana"));
-
-        fruits.set(1, "Orange");
-        System.out.println(fruits);
-
-        List<String> sliced = fruits.subList(0, 2);
-        System.out.println(sliced);
+        System.out.println(treedFruits);
     }
 }
