4b9f0bb99928729b86b2d58753ff57ff04d46119
RyoHaSeu99 <cmrhanc123@naver.com>
2025-07-01 21:35:12 +0900

    generics

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,24 @@
-import java.util.*;
+class Box<T> {
+    private T content;
 
+    public Box(T content) {
+        this.content = content;
+    }
+
+    public T getContent() {
+        return content;
+    }
+
+    public void setContent(T content) {
+        this.content = content;
+    }
+
+    public <E> void printContent(E extraInfo) {
+        System.out.println("Content: " + content + ", Extra: " + extraInfo);
+    }
+}
 public class Main {
     public static void main(String[] args) {
-        Map<String, Integer> fruitPrice = new TreeMap<>();
 
-        fruitPrice.put("Banana", 50);
-        fruitPrice.put("Cherry", 200);
-        fruitPrice.put("Apple", 150);
-
-        for (Map.Entry<String, Integer> entry : fruitPrice.entrySet()) {
-            System.out.println(entry.getKey() + " : " + entry.getValue());
-        }
     }
 }
