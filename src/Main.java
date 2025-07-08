d38de2b292912d0e9036faea54b40d62f4565eef
RyoHaSeu99 <cmrhanc123@naver.com>
2025-07-08 22:00:43 +0900

    공변 불공변

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,15 @@
-public class Main {
-    public static <T> void displayArray(T[] array) {
-        for (T element : array) {
-            System.out.println(element);
-        }
-    }
+import java.util.Arrays;
+import java.util.List;
 
+public class Main {
+    public static void printArray(Object[] arr) {
+        for (Object o : arr) System.out.println(o);
+    }
     public static void main(String[] args) {
         Integer[] numbers = {1, 2, 3, 4, 5};
-        String[] words = {"apple", "banana", "cherry"};
+        printArray(numbers);    // Integer 타입 배열을 Object 타입 배열로 처리 가능
 
-        displayArray(numbers);
-        displayArray(words);
+        List<Integer> list = Arrays.asList(1, 2, 3);
+        List<Object> objList = list;    //  오류 발생
     }
 }
