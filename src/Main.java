cf841ba50ed21a7b5cf1070553436696369baff3
RyoHaSeu99 <cmrhanc123@naver.com>
2025-07-08 16:53:33 +0900

    generics 1

--- src/Main.java
+++ src/Main.java
@@ -13,12 +13,18 @@ class Box<T> {
         this.content = content;
     }
 
+    // 제네릭 메서드 예시 (E는 메서드 전용 타입)
     public <E> void printContent(E extraInfo) {
         System.out.println("Content: " + content + ", Extra: " + extraInfo);
     }
 }
 public class Main {
     public static void main(String[] args) {
+        Box<Integer> intBox = new Box<>(123);
+        intBox.printContent("Integer 박스입니다.");
+
+        Box<String> strBox = new Box<>("Hello");
+        strBox.printContent(456);
 
     }
 }
