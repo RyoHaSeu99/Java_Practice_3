c9a69515d436313342da895d484c07639dbcb184
RyoHaSeu99 <cmrhanc123@naver.com>
2025-07-10 21:40:30 +0900

    Stream

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,12 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Main {
-    public static void printArray(Object[] arr) {
-        for (Object o : arr) System.out.println(o);
-    }
     public static void main(String[] args) {
-        Integer[] numbers = {1, 2, 3, 4, 5};
-        printArray(numbers);    // Integer 타입 배열을 Object 타입 배열로 처리 가능
-
-        List<Integer> list = Arrays.asList(1, 2, 3);
-        List<Object> objList = list;    //  오류 발생
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5);
+        int sum = numbers.stream()
+                .filter(n -> n % 2 == 0)
+                .mapToInt(Integer::intValue)
+                .sum();
+        System.out.println("Sum of even numbers: " + sum);
     }
 }
