fb917965e0fa81bf063fed46dbb03dbad851861b
RyoHaSeu99 <cmrhanc123@naver.com>
2025-07-13 15:07:31 +0900

    Runnable 3

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,39 @@
-class MyRunnable implements Runnable {
-    @Override
+class SumRunnable implements Runnable {
+    private final int[] numbers;
+
+    public SumRunnable(int[] numbers) {
+        this.numbers = numbers;
+    }
+
     public void run() {
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("Hello from Thread: " + i);
+        int sum = 0;
+        for (int n : numbers) {
+            sum += n;
             try {
-                Thread.sleep(500);
-            } catch(InterruptedException e) {
-                System.out.println("Thread was interrupted.");
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                System.out.println("Thread interrupted.");
             }
         }
-
+        System.out.println(Thread.currentThread().getName() + " - Sum: " + sum);
     }
 }
 
 public class Main {
     public static void main(String[] args) {
-        Thread runnable = new Thread(new MyRunnable());
-        runnable.start();
+        int[][] dataSets = {
+                {1, 2, 3, 4, 5},
+                {10, 20, 30, 40, 50},
+                {7, 14, 21, 28, 35},
+                {100, 200, 300, 400, 500}
+        };
+
+        for (int i = 0; i < dataSets.length; i++) {
+            Thread thread = new Thread(new SumRunnable(dataSets[i]), "Thread-" + (i + 1));
+            thread.start();
+        }
+
+        // 코드가 한 줄씩이 아닌 동시에 실행되기 때문에 sleep 메서드가 적용된 쓰레드보다 아래 코드가 먼저 실행됨
+        System.out.println("All sum threads started.");
     }
 }
