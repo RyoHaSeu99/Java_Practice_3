8374f642345f17f4c6074d0208e2ed5406976e1e
RyoHaSeu99 <cmrhanc123@naver.com>
2025-07-14 21:28:54 +0900

    어노테이션 - @SuppressWarnings

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,13 @@
-class Calculator {
-
-    @Deprecated
-    public int add(int a, int b) {
-        return a + b;
-    }
-
-    public int addNumbers(int a, int b) {
-        return a + b;
-    }
-}
+import java.util.*;
 
 public class Main {
+    @SuppressWarnings("unchecked")
     public static void main(String[] args) {
-        Calculator calc = new Calculator();
-        System.out.println(calc.add(1, 2));
-        System.out.println(calc.addNumbers(1, 2));
+       List list = new ArrayList();
+       list.add("Hello");
+       list.add("World");
+       for (Object obj : list) {
+           System.out.println(obj);
+       }
     }
 }
