2410aa98d57c371ef10d6dbb7eaa050e38b53e00
RyoHaSeu99 <cmrhanc123@naver.com>
2025-07-14 21:15:36 +0900

    어노테이션 - @Deprecated

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,19 @@
-class Animal {
-    public void sound() {
-        System.out.println("Animal makes a sound.");
-    }
-}
+class Calculator {
 
-class Dog extends Animal {
-    @Override
-    public void sound() {
-        System.out.println("Dog barks.");
+    @Deprecated
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int addNumbers(int a, int b) {
+        return a + b;
     }
 }
 
 public class Main {
     public static void main(String[] args) {
-
+        Calculator calc = new Calculator();
+        System.out.println(calc.add(1, 2));
+        System.out.println(calc.addNumbers(1, 2));
     }
 }
