25e09f158be2593487d91ec27c3e7245e7d57bcd
RyoHaSeu99 <cmrhanc123@naver.com>
2025-07-14 22:49:58 +0900

    리플렉션-getDeclaredConstructor().newInstance()

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,18 @@
 import java.lang.reflect.*;
 
 class Person {
-    public void greet(String name) {}
-    private int calculate(int x, int y) { return x + y; }
+    public Person() {
+        System.out.println("Person 생성자 호출됨");
+    }
 }
+
 public class Main {
     public static void main(String[] args) {
-        Class<?> clazz = Person.class;
-        Method[] methods = clazz.getDeclaredMethods();
-
-        for (Method method : methods) {
-            System.out.println("메서드 이름: " + method.getName());
-            for(Class<?> paramType : method.getParameterTypes()) {
-                System.out.println(" 파라미터 타입: " + paramType.getSimpleName());
-            }
+        try {
+            Class<?> clazz = Person.class;
+            Object obj = clazz.getDeclaredConstructor().newInstance();
+        } catch(Exception e) {
+            e.printStackTrace();
         }
     }
 }
