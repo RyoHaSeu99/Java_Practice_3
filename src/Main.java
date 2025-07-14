5209fbc4f4ea977d2397a53cb5353a5cf5f6f12c
RyoHaSeu99 <cmrhanc123@naver.com>
2025-07-14 22:15:13 +0900

    리플렉션-getDeclareMethods()

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,19 @@
 import java.lang.reflect.*;
 
 class Person {
-    private String name;
-    public int age;
+    public void greet(String name) {}
+    private int calculate(int x, int y) { return x + y; }
 }
-
 public class Main {
     public static void main(String[] args) {
         Class<?> clazz = Person.class;
-        Field[] fields = clazz.getDeclaredFields();
+        Method[] methods = clazz.getDeclaredMethods();
 
-        for (Field field : fields) {
-            System.out.println("필드 이름: " + field.getName());
+        for (Method method : methods) {
+            System.out.println("메서드 이름: " + method.getName());
+            for(Class<?> paramType : method.getParameterTypes()) {
+                System.out.println(" 파라미터 타입: " + paramType.getSimpleName());
+            }
         }
     }
 }
