fc8e1634db84307cf85edb3b7575f24aff75578f
RyoHaSeu99 <cmrhanc123@naver.com>
2025-07-17 01:56:39 +0900

    NIO

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,51 @@
-import java.io.FileWriter;
+import java.nio.file.*;
+import java.nio.channels.FileChannel;
+import java.nio.ByteBuffer;
 import java.io.IOException;
 
 public class Main {
-    public static void writeFile(String filename, String content) {
-        try (FileWriter writer = new FileWriter(filename)) {
-            writer.write(content);
-        } catch (IOException e) {
-            e.printStackTrace();
+    public static void writeFile() {
+        Path filePath = Paths.get("example.txt");
+        String content = "Hello, NIO World!";
+
+        try (FileChannel writeChannel = FileChannel.open(
+                filePath,
+                StandardOpenOption.CREATE,
+                StandardOpenOption.WRITE)) {
+            ByteBuffer buffer = ByteBuffer.allocate(1024);
+
+            buffer.put(content.getBytes());
+            buffer.flip();
+            writeChannel.write(buffer);
+            System.out.println("File written successfully.");
+        }  catch (IOException e) {
+            System.out.println(e.getMessage());
         }
     }
-    public static void main(String[] args) throws Exception {
-        writeFile("example.txt", "Hello, Java I/O\nThis is test.");
-        System.out.println("파일 생성 완료");
+
+    public static void readFile() {
+        Path filePath = Paths.get("example.txt");
+        try (FileChannel readChannel = FileChannel.open(
+                filePath,
+                StandardOpenOption.READ)) {
+            ByteBuffer buffer = ByteBuffer.allocate(1024);
+
+            int byteRead = readChannel.read(buffer);
+            while (byteRead != -1) {
+                buffer.flip();
+                while (buffer.hasRemaining()) {
+                    System.out.println((char) buffer.get());
+                }
+                buffer.clear();
+                byteRead = readChannel.read(buffer);
+            }
+            System.out.println("\nFile read successfully");
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+    public static void readFile(String filename) {
+
+
     }
 }
