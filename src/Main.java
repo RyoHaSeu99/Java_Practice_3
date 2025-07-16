aec44b4507723c90fbd5ef1bf56d932b730c2dba
RyoHaSeu99 <cmrhanc123@naver.com>
2025-07-17 00:38:26 +0900

    커스텀 어노테이션 2

--- src/Main.java
+++ src/Main.java
@@ -8,8 +8,28 @@ import java.lang.annotation.*;
     int version() default 1;
 }
 
+@CustomInfo(author = "John Doe", date = "2025-07-17", version = 2)
+class Demo {
+    // 내부 로직
+}
+
+
+
 public class Main {
+
+    @CustomInfo(author = "Jane Smith", date = "2025-07-17")
+    public void display() {
+        System.out.println("Display method execute.");
+    }
+
     public static void main(String[] args) throws Exception {
+        Class<?> demoClass = demo.getClass();
+        if (demoClass.isAnnotationPresent(CustomInfo.class));
+        CustomInfo info = demoClass.getAnnotation(CustomInfo.class);
+        System.out.println("Class -> Author: " + classInfo.author() +
+                ", Date: " + classInfo.date() +
+                ", Version: " + classInfo.version());
+
 
     }
 }
