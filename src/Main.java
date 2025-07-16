d17874e479852469140300143dbe8c7c3c27b09b
RyoHaSeu99 <cmrhanc123@naver.com>
2025-07-16 23:44:05 +0900

    리플렉션-getDeclaredMethod()

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,18 @@
 import java.lang.reflect.*;
 
-class Calculator {
-    public int add(int a, int b) {
-        return a + b;
+class Secret {
+    private void whisper() {
+        System.out.println("This is a secret method.");
     }
 }
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        Class<?> clazz = Calculator.class;
+        Class<?> clazz = Secret.class;
         Object instance = clazz.getDeclaredConstructor().newInstance();
 
-        Method method = clazz.getMethod("add", int.class, int.class);
-        Object result = method.invoke(instance, 10, 20);
-
-        System.out.println("결과: " + result);
+        Method method = clazz.getDeclaredMethod("whisper");
+        method.setAccessible(true); // private 접근 허용
+        method.invoke(instance);    // 메서드 실행
     }
 }
